3ee69ea0c21184a0dde374c45e845d0ee27ade55
le_sambestia <515985@chrome.escg.ac.uk>
2021-03-22 11:08:25 +0000

    finished the turret shooting mechanics

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,6 +4,7 @@ public class Bullet : MonoBehaviour
 {
     private Transform target;
     public float speed = 70f;
+    public GameObject impactEffect;
 
     public void Seek(Transform _target)
     {
@@ -31,6 +32,8 @@ public class Bullet : MonoBehaviour
 
     void HitTarget()
     {
+        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 2f);
         Destroy(gameObject);
     }
 }
