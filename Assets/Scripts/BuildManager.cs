911eb83db10660413ffba26346a80d2760c22b11
le_sambestia <515985@chrome.escg.ac.uk>
2021-03-25 16:14:46 +0000

    added turret placements and node management

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("More than one BuildManager");
+            return;
+        }
+        instance = this;
+    }
+    public GameObject StandardTurretPrefab;
+
+    void Start()
+    {
+        turretToBuild = StandardTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
