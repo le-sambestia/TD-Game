e7e814eb2d3a916b8e8979391d28488fa8d85482
le_sambestia <515985@chrome.escg.ac.uk>
2021-05-27 07:24:34 +0100

    bug fixes

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -11,7 +11,6 @@ public class GameOver : MonoBehaviour
 
     public SceneFader sceneFader;
 
-
     public void Retry()
     {
         sceneFader.FadeTo(SceneManager.GetActiveScene().name);
--- Assets/Scripts/pauseMenu.cs
+++ Assets/Scripts/pauseMenu.cs
@@ -23,15 +23,6 @@ public class pauseMenu : MonoBehaviour
     public void Toggle()
     {
         ui.SetActive(!ui.activeSelf);
-
-        if (ui.activeSelf)
-        {
-            Time.timeScale = 0f;
-        }
-        else
-        {
-            Time.timeScale = 1f;
-        }
     }
 
     public void Retry()
